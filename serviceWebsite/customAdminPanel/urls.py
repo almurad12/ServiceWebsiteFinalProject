c19a8b2e4e362ce14eb8a4244b5a34795fba728b
almurad12 <murad.ahmed061@gmail.com>
2023-01-16 10:30:23 +0600

    3rd vestion

--- serviceWebsite/customAdminPanel/urls.py
+++ serviceWebsite/customAdminPanel/urls.py
@@ -3,6 +3,16 @@ from customAdminPanel import views
 urlpatterns = [
     ###working on dashboard
     path('newadminpanel', views.newadminpanel,name='dashboard'),
-    path('servicelist', views.userlist,name='servicelist'),
-    path('alluserlist', views.alluserlist,name='userlist'),
+    path('userlist', views.userlist,name='userlist'),
+    path('userlist/<id>/delete', views.userlistdelete,name='userlistdelete'),
+    ##allservicelist
+    path('allservicelist',views.servicelist,name="allservicelist"),
+    path('allservicelist/delete',views.servicelistdelete),
+    ##all pending order
+    path('allpendingorderlist', views.allpendingorderlist,name='allpendingorderlist'),
+    path('allpendingorderlist/delete', views.pendingorderdelete,name='pendingorderdelete'),
+    ##all order list
+    path('allorderlist', views.allorderlist,name='allorderlist'),
+    path('allorderlist/delete',views.allorderdelete,name="allorderdelete")
+    # path('orderlist', views.orderlist,name='orderlist'),
 ]
--- serviceWebsite/customAdminPanel/views.py
+++ serviceWebsite/customAdminPanel/views.py
@@ -1,11 +1,15 @@
 
 # Create your views here.
-from django.shortcuts import render,HttpResponseRedirect,redirect
+from django.shortcuts import render,HttpResponseRedirect,redirect,get_object_or_404
 from  django.contrib.auth.models import User
 from django.contrib.auth import authenticate,login
 from django.contrib import messages
 from account.models import User
+from cartanother.models import Cartanothernew
+from cartanother.models import Ordernew
+from service.models import Sheba
 from .forms import UserForm
+
 
 # def adminlogin(request):
 #     try:
@@ -32,12 +36,12 @@ from .forms import UserForm
     # return redirect('')
 
 def newadminpanel(request):
-    context ={}
-    form = UserForm(request.POST or None)
-    if form.is_valid():
-        form.save()
-    context['form']= form
-    return render(request,'adminpanel/cartDashboard.html',context)
+    # context ={}
+    # form = UserForm(request.POST or None)
+    # if form.is_valid():
+    #     form.save()
+    # context['form']= form
+    return render(request,'adminpanel/cartDashboard.html')
     #  return render(request,'adminpanel/cartDashboard.html')
 
 def userlist(request):
@@ -47,7 +51,72 @@ def userlist(request):
         form.save()
     context['form']= form
     context['alluserlist']= User.objects.all()
+    return render(request,'adminpanel/alluserlist.html',context)
+
+def userlistdelete(request,id):
+    if request.method =='POST':
+          user =User.objects.get(pk=id)
+          print(user)
+          user.delete()
+
+    # context ={}
+    # obj = get_object_or_404(User, id = id)
+    # if request.method =="POST":
+            # delete object
+            # obj.delete()
+            # after deleting redirect to
+            # home page
+            # context['alluserlist']= User.objects.all()
+    return HttpResponseRedirect('/customadmin/servicelist')
+    #         return HttpResponseRedirect("/")
+    # return render(request, "adminpanel/allservicelist.html", context)
+
+# def orderlist(request):
+#     return render(request,'adminpanel/alluserlist.html')
+
+##all servicelist
+def servicelist(request):
+    context={}
+    context['allservicelist']=Sheba.objects.all()
     return render(request,'adminpanel/allservicelist.html',context)
+
+def servicelistdelete(request):
+    if request.method=='GET':
+        id = request.GET.get('id')
+        serviceid = Sheba.objects.get(id=id)
+        print(serviceid)
+        serviceid.delete()
+        return HttpResponseRedirect('/customadmin/allservicelist')
+    
+def allpendingorderlist(request):
+    context ={}
+    context['allpendingorder']= Cartanothernew.objects.all()
+    return render(request,'adminpanel/allpendingorder.html',context)
+
+def pendingorderdelete(request):
+     if request.method =='GET':
+        id = request.GET.get('userid')
+        print(id)
+        pendingorder = Cartanothernew.objects.get(id=id)
+        print(pendingorder)
+        pendingorder.delete()
+        return HttpResponseRedirect('/customadmin/allpendingorderlist')
+
+
+###all orderlist
+def allorderlist(request):
+   context={}
+   context['allorderlist']= Ordernew.objects.all()
+   return render(request,'adminpanel/allorderlist.html',context)
+
+def allorderdelete(request):
+    if request.method =='GET':
+        id = request.GET.get('orderid')
+        print(id)
+        pendingorder = Ordernew.objects.get(id=id)
+        print(pendingorder)
+        pendingorder.delete()
+        return HttpResponseRedirect('/customadmin/allorderlist')
 
 def alluserlist(request):
     return render(request,'adminpanel/alluserlist.html')
